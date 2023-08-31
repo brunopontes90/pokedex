af22a92fcd91fedbf6bb5539c24531e31d0b40d7
Bruno Pontes <bruno_confessor@hotmail.com>
2023-08-31 19:19:13 -0300

    Chamada de API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [listPokemon, setListPokemon] = useState([]);
+
+  useEffect(()=>{
+    Axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    .then((response)=>{console.log(response.data.results)})
+  });
   return (
     <div>
       <h1>Teste</h1>
